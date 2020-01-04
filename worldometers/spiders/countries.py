740956e72f2e17cd67d06c1040ae7d0db2b8dd60
hirenvadher954 <hirenvadher954@gmail.com>
2020-01-04 15:09:47 +0530

    V 1.0

--- worldometers/spiders/countries.py
+++ worldometers/spiders/countries.py
@@ -13,4 +13,16 @@ class CountriesSpider(scrapy.Spider):
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
-            yield response.follow(url=link)
+            yield response.follow(url=link, callback=self.parse_country)
+
+    def parse_country(self, response):
+        rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed "
+                              "table-list'])[1]/tbody/tr")
+        for row in rows:
+            year = rows.xpath(".//td[1]/text()").get()
+            population = rows.xpath(".//td[2]/strong/text()").get()
+
+            yield {
+                'year': year,
+                'population': population
+            }
